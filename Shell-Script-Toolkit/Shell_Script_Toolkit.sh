b3fe84ad0a417779624badf41313ea5467cef32b
Oleg Chashko <62497891+Oleg-Chashko@users.noreply.github.com>
2021-08-03 10:42:41 +0200

    Update UI

--- Shell-Script-Toolkit/Shell_Script_Toolkit.sh
+++ Shell-Script-Toolkit/Shell_Script_Toolkit.sh
@@ -464,9 +464,9 @@ settingHostnameAndInformationAboutTheNewCurrentHostname() {
 netStatusPingIPv4() {
     echo "•${F_Red} You choose to net Status Ping IPv4.${No_Attributes}"
     declare -a arr=("Google.com" "Youtube.com" "Facebook.com" "Instagram.com" "8.8.8.8" "8.8.4.4")
-    echo "\n**************************************************************************************************\n"
-    echo "* Host or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds *"
-    echo "\n**************************************************************************************************\n"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
+    echo "${Dim}•${No_Attributes} Host or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds ${Dim}•${No_Attributes}"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
     echo "•${F_Red} Testing...\n${No_Attributes}"
     for i in "${arr[@]}"; do
         time=$(ping -c 5 "$i" | tail -1 | awk '{print $4}')
@@ -480,9 +480,9 @@ netStatusPingIPv4() {
 netStatusPingIPv6() {
     echo "•${F_Red} You choose to net Status Ping IPv6.${No_Attributes}"
     declare -a arr=("Google.com" "Youtube.com" "Facebook.com" "Instagram.com" "2001:4860:4860::8888" "2001:4860:4860::8844")
-    echo "\n**************************************************************************************************\n"
-    echo "* Host or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds *"
-    echo "\n**************************************************************************************************\n"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
+    echo "${Dim}•${No_Attributes} Host or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds ${Dim}•${No_Attributes}"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
     echo "•${F_Red} Testing...\n${No_Attributes}"
     for i in "${arr[@]}"; do
         time=$(ping6 -c 5 "$i" | tail -1 | awk '{print $4}')
@@ -496,9 +496,9 @@ netStatusPingIPv6() {
 findingRoutersOnLocalNetworks() {
     echo "•${F_Red} You choose to Finding Routers on Local Networks.${No_Attributes}"
     declare -a arr=("10.0.0.1" "10.0.0.2" "10.0.0.138" "10.0.1.1" "10.1.1.1" "10.1.10.1" "10.10.1.1" "10.90.90.90" "192.168.0.1" "192.168.0.3" "192.168.0.10" "192.168.0.30" "192.168.0.50" "192.168.0.100" "192.168.0.101" "192.168.0.227" "192.168.0.254" "192.168.1.1" "192.168.1.10" "192.168.1.99" "192.168.1.100" "192.168.1.200" "192.168.1.210" "192.168.1.254" "192.168.2.1" "192.168.2.254" "192.168.3.1" "192.168.4.1" "192.168.8.1" "192.168.10.1" "192.168.10.10" "192.168.10.50" "192.168.10.100" "192.168.11.1" "192.168.15.1" "192.168.16.1" "192.168.20.1" "192.168.30.1" "192.168.50.1" "192.168.55.1" "192.168.62.1" "192.168.100.1" "192.168.100.100" "192.168.102.1" "192.168.123.254" "192.168.168.168" "192.168.223.100" "192.168.251.1" "192.168.254.254" "200.200.200.5")
-    echo "\n****************************************************************************************************\n"
-    echo "* Router or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds *"
-    echo "\n****************************************************************************************************\n"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
+    echo "${Dim}•${No_Attributes} Router or IP address | Response time: Minimum/Average/Maximum/Standard_deviation in milliseconds ${Dim}•${No_Attributes}"
+    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
     echo "•${F_Red} Finding Routers on Local Networks...\n${No_Attributes}"
     for i in "${arr[@]}"; do
         time=$(ping -c 5 "$i" | tail -1 | awk '{print $4}')
