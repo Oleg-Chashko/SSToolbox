9cef8e6f95630a6f537bd108aeb45d0aa7cb8014
Oleg Chashko <62497891+Oleg-Chashko@users.noreply.github.com>
2021-09-02 13:57:20 +0200

    Update: Com.14 Search Routers on local Networks

--- SSToolkit_Nightly/SSToolkit_Nightly.sh
+++ SSToolkit_Nightly/SSToolkit_Nightly.sh
@@ -852,17 +852,12 @@ settingDefaultComputerName() {
 # Command 14: Search Routers on local Networks, uses the default IP addresses
 searchRoutersLocalNetworks() {
     terminalWindowSize55x100
-    echo "•${F_Red}${Bold} Command 14 You choose to Search Routers on local Networks, uses the default IP addresses.${No_Attributes}"
+    echo "•${F_Red}${Bold} Command 14 You choose to Search Routers on local Networks, uses the default IP addresses.${No_Attributes}\n"
     declare -a arr=("10.0.0.1" "10.0.0.2" "10.0.0.138" "10.0.1.1" "10.1.1.1" "10.1.10.1" "10.10.1.1" "10.90.90.90" "192.168.0.1" "192.168.0.3" "192.168.0.10" "192.168.0.30" "192.168.0.50" "192.168.0.100" "192.168.0.101" "192.168.0.227" "192.168.0.254" "192.168.1.1" "192.168.1.10" "192.168.1.99" "192.168.1.100" "192.168.1.200" "192.168.1.210" "192.168.1.254" "192.168.2.1" "192.168.2.254" "192.168.3.1" "192.168.4.1" "192.168.8.1" "192.168.10.1" "192.168.10.10" "192.168.10.50" "192.168.10.100" "192.168.11.1" "192.168.15.1" "192.168.16.1" "192.168.20.1" "192.168.30.1" "192.168.50.1" "192.168.55.1" "192.168.62.1" "192.168.100.1" "192.168.100.100" "192.168.102.1" "192.168.123.254" "192.168.168.168" "192.168.223.100" "192.168.251.1" "192.168.254.254" "200.200.200.5")
-    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
-    echo "${Dim}•${No_Attributes} Router- or IP- address | Response time: Minimum/Average/Maximum/Standard_deviation ${Dim}•${No_Attributes}"
-    echo "\n${Dim}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••${No_Attributes}\n"
-    echo "•${F_Red} Finding Routers on Local Networks...\n${No_Attributes}"
     for i in "${arr[@]}"; do
         time=$(ping -W 800 -i 0.1 -c 4 "$i" | tail -1 | awk '{print $4}')
         echo "$i | response time | $time ms"
     done
-    echo "•${F_Red} Finish...${No_Attributes}"
     continueMessage
 }
 
